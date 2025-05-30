2757911fecc66e0dc8259806ea3572893e981c73
Isaac Lum <zisaac2468@gmail.com>
2025-05-31 02:15:27 +0800

    Merged CalendarPage and LoginAndRegister branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
+
+import ProtectedRoute from './components/protectedRoute';
 import Default from './components/Default';
 import Login from './components/Login';
 import Register from './components/Register';
 import HomePage from './components/HomePage';
-import ProtectedRoute from './components/protectedRoute';
+import CalendarDashboard from './components/CalendarDashboard';
 function App() {
   return (
     <Router>
@@ -14,7 +16,8 @@ function App() {
 
         {/*Protected routes*/}
         <Route element={<ProtectedRoute />}>
-          <Route path="/home" element={<HomePage />} />
+          {/* <Route path="/home" element={<HomePage />} /> */}
+          <Route path="/home" element={<CalendarDashboard />} />
         </Route>
         {/* <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} /> */}
       </Routes>
